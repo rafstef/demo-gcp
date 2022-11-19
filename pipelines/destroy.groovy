ebf6138a053da55a889c5f171035f7636eff5f72
Raffaele Stefanile <raffaele.stefanile@capgemini.com>
2022-11-19 16:36:59 +0100

    test

--- pipelines/destroy.groovy
+++ pipelines/destroy.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call (String message,String multipleChoise,String defaultChoice,int time) {
+def askUserInput(String message,String multipleChoise,String defaultChoice,int time) {
     def userInput = defaultChoice
     try{
         timeout(time: time, unit: 'SECONDS') {
@@ -12,42 +12,75 @@ def call (String message,String multipleChoise,String defaultChoice,int time) {
     userInput
 }
 
+def getEnvName(branchName) {
+    if("origin/develop".equals(branchName)) {
+        return "DEV";
+    } else if ("origin/master".equals(branchName)) {
+        return "PROD";
+    } else if ("origin/release".equals(branchName)) {
+        return "PREPROD";
+    }
+}
+
 pipeline {
     agent any
+    environment {
+        TERRAFORM_PLAN_FILE="DEMO"
+        ENV_NAME = getEnvName(env.GIT_BRANCH)
+    }
     tools {
         terraform 'terraform'
     }
     stages{
+        stage ("Setup Deployment Environment") {
+            steps {
+                script {
+                    echo "branch name: ${env.GIT_BRANCH}"
+                    echo "env name: ${env.ENV_NAME.toLowerCase()}"
+                    load "envvars/${env.ENV_NAME.toLowerCase()}.groovy"
+                }
+            }
+        }
+
         stage('Login Step') {
             steps {
-                withCredentials([file(credentialsId: "GCP_SERVICE_ACCOUNT", variable: 'BACKEND_CREDENTIALS')]){
+                withCredentials([file(credentialsId: "GCP_SERVICE_ACCOUNT", variable: 'GCP_CREDENTIALS')]){
                     script {
                         sh "mkdir -p credentials"
-                        sh "cat $GCP_CREDENTIALS > credentias/gcp-credentials.json"
+                        sh "cat $GCP_CREDENTIALS > credentials/gcp-credentials.json"
                         env.GOOGLE_APPLICATION_CREDENTIALS="credentials/gcp-credentials.json"
                     }
-                }  
+                }
             }
         }
         stage('TF Init and validations checks') {
             steps {
                 sh "terraform version"
                 sh "terraform init -no-color"
+                sh "terraform workspace select ${env.ENV_NAME} -no-color"
                 sh "terraform validate -no-color"
             }
         }
         stage('TF Plan creation') {
             steps {
+                sh "terraform version"
+                sh "terraform init -no-color"
+                sh "terraform workspace select ${env.ENV_NAME} -no-color"
                 sh "terraform plan -destroy -no-color -input=false -out terraform-plan-${env_name}.plan"
             }
         }
         stage('TF Destroy') {
             steps {
-                approve_plan=buildStep_askUserInput("Apply Terraform plan?","NO\nYES","NO",300)
-                if( approve_plan == "YES"){
-                    sh "terraform apply -destroy -no-color -input=false terraform-plan-${env_name}.plan"
-                }else{
-                    echo "TF destroy not approved. Skip Apply . . . "
+                script{
+                    approve_plan=askUserInput("Apply Terraform destroy plan?","NO\nYES","NO",300)
+                    if( approve_plan == "YES"){
+                        sh "terraform version"
+                        sh "terraform init -no-color"
+                        sh "terraform workspace select ${env.ENV_NAME} -no-color"
+                        sh "terraform apply -destroy -no-color -input=false terraform-plan-${env_name}.plan"
+                    }else{
+                        echo "TF destroy plan not approved. Skip Apply . . . "
+                    }
                 }
             }
         }
